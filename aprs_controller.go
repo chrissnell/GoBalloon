bc835244480b80cd53c9071499bea65537a3730e
Christopher Snell <chris.snell@gmail.com>
2014-06-15 17:25:37 -0700

    Use remote tnc-server to send APRS packets

--- aprs_controller.go
+++ aprs_controller.go
@@ -6,7 +6,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/chrissnell/GoBalloon/aprs"
+	"github.com/chrissnell/GoBalloon/ax25"
+	"github.com/chrissnell/GoBalloon/geospatial"
+	"log"
+	"net"
 	"time"
 )
 
@@ -14,8 +20,21 @@ var (
 	aprsMessage = make(chan string, 10)
 )
 
-func aprsBeacon() {
+func aprsBeacon(aprssource, aprsdest ax25.APRSAddress) {
 	fmt.Println("--- sendAPRSBeacon start")
+
+	var path []ax25.APRSAddress
+
+	path = append(path, ax25.APRSAddress{
+		Callsign: "WIDE1",
+		SSID:     1,
+	})
+
+	path = append(path, ax25.APRSAddress{
+		Callsign: "WIDE2",
+		SSID:     1,
+	})
+
 	for {
 		select {
 		case <-timeToDie:
@@ -27,8 +46,52 @@ func aprsBeacon() {
 			if currentPosition.Lat != 0 || currentPosition.Lon != 0 {
 				var amsg = fmt.Sprintf("hi lat=%v lon=%v alt=%v", currentPosition.Lat, currentPosition.Lon, currentPosition.Alt)
 				aprsMessage <- amsg
+
+				point := geospatial.NewPoint()
+				point.Lat = currentPosition.Lat
+				point.Lon = currentPosition.Lon
+				point.Alt = currentPosition.Alt
+
+				position := aprs.CreateCompressedPosition(point, '/', 'O')
+				body := fmt.Sprint(position, "GoBalloon Test http://nw5w.com")
+
+				a := ax25.APRSData{
+					Source: aprssource,
+					Dest:   aprsdest,
+					Path:   path,
+					Body:   body,
+				}
+
+				packet, err := ax25.EncodeAX25Command(a)
+				if err != nil {
+					log.Fatalf("Unable to create packet: %v", err)
+				}
+
+				conn, err := net.Dial("tcp", *remotetnc)
+				if err != nil {
+					log.Fatalf("Could not connect to %v.  Error: %v", *remotetnc, err)
+				}
+
+				bw, err := conn.Write(packet)
+				if err != nil {
+					log.Fatalf("Could not write to remote.  Error: %v", err)
+				} else {
+					log.Printf("Wrote %v bytes to %v", bw, conn.RemoteAddr())
+				}
+
+				err = conn.Close()
+				if err != nil {
+					log.Fatalf("Error closing connection: %v", err)
+				}
+
+				// Let's decode our own packet to make sure tht it's bueno
+				buf := bytes.NewReader(packet)
+				d := ax25.NewDecoder(buf)
+				msg, err := d.Next()
+				fmt.Printf("%+v\n", msg)
+
 			}
-			timer := time.NewTimer(time.Second * 10)
+			timer := time.NewTimer(time.Second * 30)
 			<-timer.C
 
 		}
--- goballoon.go
+++ goballoon.go
@@ -6,7 +6,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/chrissnell/GoBalloon/ax25"
 	"github.com/chrissnell/GoBalloon/geospatial"
 	"os"
 	"os/signal"
@@ -16,16 +18,34 @@ import (
 var (
 	timeToDie       = make(chan bool, 1)
 	currentPosition geospatial.Point
+	remotegps       *string
+	remotetnc       *string
 )
 
 func main() {
+
+	remotegps = flag.String("remotegps", "10.50.0.21:2947", "Remote gpsd server")
+	remotetnc = flag.String("remote", "10.50.0.25:6700", "Remote TNC server")
+
+	flag.Parse()
+
 	fmt.Println("Starting up.")
 
 	sc := make(chan os.Signal, 2)
 	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
 
+	aprssource := ax25.APRSAddress{
+		Callsign: "NW5W",
+		SSID:     7,
+	}
+
+	aprsdest := ax25.APRSAddress{
+		Callsign: "APZ001",
+		SSID:     0,
+	}
+
 	go CameraRun()
-	go aprsBeacon()
+	go aprsBeacon(aprssource, aprsdest)
 	go GPSRun()
 	<-sc
 	timeToDie <- true
