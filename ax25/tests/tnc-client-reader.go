5ad5b5dc5520e70934307e5e964948548c5dc318
Christopher Snell <chris.snell@gmail.com>
2014-06-17 22:07:32 -0700

    Deal with dropped connections and malformed packets in a better way

--- ax25/tests/tnc-client-reader.go
+++ ax25/tests/tnc-client-reader.go
@@ -13,6 +13,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	_ "time"
 	// "github.com/chrissnell/go-base91"
 )
 
@@ -24,28 +25,38 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
 
-	conn, err := net.Dial("tcp", *remote)
-	if err != nil {
-		log.Fatalf("Could not connect to %v.  Error: %v", *remote, err)
-	}
-
-	defer conn.Close()
-
 	go func() {
 		<-sig
 		os.Exit(1)
 	}()
 
-	d := ax25.NewDecoder(conn)
-
 	for {
-		msg, err := d.Next()
 
+		log.Printf("Connecting to %v...\n", *remote)
+		conn, err := net.Dial("tcp", *remote)
 		if err != nil {
-			log.Printf("Error retrieving APRS message via KISS: %v", err)
+			log.Fatalf("Could not connect to %v.  Error: %v", *remote, err)
 		}
 
-		fmt.Printf("%+v\n", msg)
+		d := ax25.NewDecoder(conn)
+
+		for {
+			msg, err := d.Next()
+
+			if err != nil {
+				log.Printf("Error retrieving APRS message via KISS: %v", err)
+				// log.Println("Sleeping 5 seconds and reconnecting.")
+				// timer := time.NewTimer(time.Second * 5)
+				// <-timer.C
+				// break
+
+			}
+
+			fmt.Printf("%+v\n", msg)
+		}
+
+		log.Println("Closing connection.")
+		conn.Close()
 
 	}
 }
