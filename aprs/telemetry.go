dce2ff42d23ce30b829bca40009f74f78b676df0
Christopher Snell <chris.snell@gmail.com>
2014-06-30 00:03:16 -0700

    Simplified telemetry data structures

--- aprs/telemetry.go
+++ aprs/telemetry.go
@@ -9,50 +9,87 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type TelemetryReport struct {
-	Analog  map[string]uint8
+type StdTelemetryReport struct {
+	A1      uint8
+	A2      uint8
+	A3      uint8
+	A4      uint8
+	A5      uint8
 	Digital byte
 }
 
-func CreateTelemetryReport(r *TelemetryReport) string {
-	var buffer bytes.Buffer
+type CompressedTelemetryReport struct {
+	A1      uint16
+	A2      uint16
+	A3      uint16
+	A4      uint16
+	A5      uint16
+	Digital byte
+}
 
-	tk := make([]string, len(r.Analog))
-
-	i := 0
-	for _, v := range r.Analog {
-		if i < 5 {
-			tk[i] = fmt.Sprintf("%03v", int(v))
-		}
-		i++
-	}
-	sort.Strings(tk)
-
+func CreateTelemetryReport(r *StdTelemetryReport) string {
 	// First byte in our telemetry report is the data type indicator.
 	// The rune 'T' indicates a standard APRS telemetry report with
 	// five analog values and one digital value
-	buffer.WriteRune('T')
-
-	buffer.WriteString(strings.Join(tk, ","))
-
-	buffer.WriteString(fmt.Sprintf(",%08b", r.Digital))
-
-	return buffer.String()
+	return fmt.Sprintf("T%03v,%03v,%03v,%03v,%03v,%08b", r.A1, r.A2, r.A3, r.A4, r.A5, r.Digital)
 }
 
-func ParseTelemetryReport(s string) (*TelemetryReport, error) {
+func CreateCompressedTelemetryReport(seq uint16, r *CompressedTelemetryReport) (string, error) {
+	var buffer bytes.Buffer
+
+	buffer.WriteRune('|')
+
+	seq = (seq + 1) & 0x1FFF
+
+	sv, err := EncodeBase91Telemetry(seq)
+	if err != nil {
+		return "", err
+	}
+	buffer.Write(sv)
+
+	A1e, err := EncodeBase91Telemetry(r.A1)
+	if err != nil {
+		return "", err
+	}
+	A2e, err := EncodeBase91Telemetry(r.A2)
+	if err != nil {
+		return "", err
+	}
+	A3e, err := EncodeBase91Telemetry(r.A3)
+	if err != nil {
+		return "", err
+	}
+
+	A4e, err := EncodeBase91Telemetry(r.A4)
+	if err != nil {
+		return "", err
+	}
+
+	A5e, err := EncodeBase91Telemetry(r.A5)
+	if err != nil {
+		return "", err
+	}
+
+	buffer.Write(A1e)
+	buffer.Write(A2e)
+	buffer.Write(A3e)
+	buffer.Write(A4e)
+	buffer.Write(A5e)
+
+	return buffer.String(), nil
+
+}
+
+func ParseTelemetryReport(s string) (*StdTelemetryReport, error) {
 	var err error
 	var errMissingTelemElements = errors.New("Telemetry message has incorrect number of elements")
 	var telems []string
 
-	r := &TelemetryReport{}
-
-	tmap := make(map[string]uint8)
+	r := &StdTelemetryReport{}
 
 	telems = strings.Split(s[1:], ",")
 	if len(telems) != 6 {
@@ -60,16 +97,16 @@ func ParseTelemetryReport(s string) (*TelemetryReport, error) {
 		return nil, err
 	}
 
-	for i := 0; i < 5; i++ {
-		ti, err := strconv.Atoi(telems[i])
-		if err != nil {
-			err = errors.New("Unable to convert telemetry value to unsigned integer (must be a whole number)")
-			return nil, err
-		}
-		tmap[strconv.Itoa(i)] = uint8(ti)
-	}
-
-	r.Analog = tmap
+	a1, err := strconv.Atoi(telems[0])
+	r.A1 = uint8(a1)
+	a2, err := strconv.Atoi(telems[1])
+	r.A2 = uint8(a2)
+	a3, err := strconv.Atoi(telems[2])
+	r.A3 = uint8(a3)
+	a4, err := strconv.Atoi(telems[3])
+	r.A4 = uint8(a4)
+	a5, err := strconv.Atoi(telems[4])
+	r.A5 = uint8(a5)
 
 	di, err := strconv.Atoi(telems[5])
 	r.Digital = byte(di)
