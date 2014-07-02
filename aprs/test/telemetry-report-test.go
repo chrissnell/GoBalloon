37eb0953f3d3c0acfaf083c601fce1f01e2e9b90
Christopher Snell <chris.snell@gmail.com>
2014-07-01 21:32:29 -0700

    test new functionality

--- aprs/test/telemetry-report-test.go
+++ aprs/test/telemetry-report-test.go
@@ -8,35 +8,45 @@ import (
 
 func main() {
 	s := aprs.StdTelemetryReport{
-		A1:      206,
-		A2:      100,
-		A3:      20,
-		A4:      36,
-		A5:      40,
-		Digital: 77,
+		Sequence: 1,
+		A1:       206,
+		A2:       100,
+		A3:       20,
+		A4:       36,
+		A5:       40,
+		Digital:  77,
 	}
 
 	c := aprs.CompressedTelemetryReport{
-		A1:      7740,
-		A2:      105,
-		A3:      20,
-		A4:      3006,
-		A5:      403,
-		Digital: 77,
+		Sequence: 3,
+		A1:       7740,
+		A2:       105,
+		A3:       20,
+		A4:       3006,
+		A5:       403,
+		Digital:  77,
 	}
 
-	tr := aprs.CreateTelemetryReport(&s)
-	ctr, err := aprs.CreateCompressedTelemetryReport(901, &c)
+	tr := aprs.CreateUncompressedTelemetryReport(&s)
+	fmt.Printf("Standard telemetry structure: %v\n", tr)
+	p, err := aprs.ParseUncompressedTelemetryReport(tr)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	fmt.Printf("Standard telemetry report%v\n", p)
+
+	fmt.Printf("Compressed telemetry structure: %+v\n", c)
+	ctr, err := aprs.CreateCompressedTelemetryReport(&c)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 
-	fmt.Printf("Telemetry report: %v\n", tr)
-	fmt.Printf("Compressed Telemetry report: %v\n", ctr)
+	fmt.Printf("Compressed telemetry report: %v\n", ctr)
 
-	p, err := aprs.ParseTelemetryReport(tr)
+	pc, err := aprs.ParseCompressedTelemetryReport(ctr)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
-	fmt.Printf("%v\n", p)
+	fmt.Printf("Decompressed compressed telemetry report: %+v\n", pc)
+
 }
