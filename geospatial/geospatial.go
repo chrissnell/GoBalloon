56c6d93789a4a98624434495d85765f3ed2677cd
Christopher Snell <chris.snell@gmail.com>
2014-07-01 21:33:30 -0700

    Add support for APRS uncompressed position formatting

--- geospatial/geospatial.go
+++ geospatial/geospatial.go
@@ -5,10 +5,17 @@
 
 package geospatial
 
+import (
+	"fmt"
+	"math"
+)
+
 type Point struct {
-	Lat float64
-	Lon float64
-	Alt float64
+	Lat      float64
+	Lon      float64
+	Altitude float64
+	Speed    float64
+	Heading  uint16
 }
 
 func NewPoint() *Point {
@@ -18,4 +25,20 @@ func NewPoint() *Point {
 func (p *Point) DistanceTo(t *Point) (d uint32) {
 	d = 1234
 	return d
+}
+
+// APRS latitude format:  DDMM.mm
+func LatDecimalDegreesToDegreesDecimalMinutes(d float64) string {
+	deg := math.Floor(d)
+	min := (d - deg) * 60
+	ddm := fmt.Sprintf("%02d%02.2f", int(deg), min)
+	return ddm
+}
+
+// APRS longitude format: DDDMM.mm
+func LonDecimalDegreesToDegreesDecimalMinutes(d float64) string {
+	deg := math.Floor(d)
+	min := (d - deg) * 60
+	ddm := fmt.Sprintf("%03d%02.2f", int(deg), min)
+	return ddm
 }
